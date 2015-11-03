ac1312fcb60b8eb08a3b4a28e03027af8571a453
Dave Hardiman <dave.hardiman@ordnancesurvey.co.uk>
2015-11-03 08:28:30 +0000

    Export the required methods for swift

--- DHCoreData/NSManagedObject+DHSwiftSupport.h
+++ DHCoreData/NSManagedObject+DHSwiftSupport.h
@@ -5,17 +5,10 @@
 //  Created by Dave Hardiman on 02/11/2015.
 //
 //
+@import CoreData;
 
 @interface NSManagedObject (DHSwiftSupport)
 
-+ (NSArray *)objectsInContext:(NSManagedObjectContext *)context
-        sortedWithDescriptors:(NSArray *)sortDescriptors
-            matchingPredicate:(id)stringOrPredicate
-                    arguments:(va_list)args;
-
-+ (instancetype)objectInContext:(NSManagedObjectContext *)context
-          sortedWithDescriptors:(NSArray *)sortDescriptors
-              matchingPredicate:(id)stringOrPredicate
-                      arguments:(va_list)args;
++ (NSEntityDescription *)entityDescriptionForClassInContext:(NSManagedObjectContext *)context;
 
 @end
